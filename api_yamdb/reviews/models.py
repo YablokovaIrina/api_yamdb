e0a6cdc581358c050acbffa12d6e80ef576bd364
Yablokova Irina <yablokovairinav@yandex.ru>
2023-01-24 18:46:11 +0300

    Добавление verbose name в модели title, genre, category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50, unique=True)
+    name = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Идентификатор')
 
     class Meta:
         ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
@@ -14,36 +19,46 @@ class Category(models.Model):
 
 class Genre(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Идентификатор')
 
     class Meta:
         ordering = ('name',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name = models.TextField(max_length=256)
-    year = models.IntegerField()
+    name = models.TextField(max_length=256, verbose_name='Название')
+    year = models.IntegerField(verbose_name='Год выпуска')
     description = models.TextField(
         blank=True,
-        null=True
+        null=True,
+        verbose_name='Описание'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='titles'
+        related_name='titles',
+        verbose_name='Категория'
     )
     genre = models.ManyToManyField(
         Genre,
-        related_name='titles'
+        related_name='titles',
+        verbose_name='Жанры'
     )
 
     class Meta:
         ordering = ('name',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
 
     def str(self):
         return self.name
