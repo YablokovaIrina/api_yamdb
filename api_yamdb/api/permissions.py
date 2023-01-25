418c0b803426c9f469ffbf63d0dc8b8d53613e25
Yablokova Irina <yablokovairinav@yandex.ru>
2023-01-25 13:35:43 +0300

    Добавление views, permissions, serializers, filters

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,21 @@
+ from rest_framework import permissions
+
+
+class AdminPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated
+                and request.user.is_admin)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user.is_authenticated
+                and request.user.is_admin)
+
+
+class ReadOnlyPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import filters, permissions, status, viewsets, mixins
+from rest_framework.response import Response
 
-# Create your views here.
+from reviews.models import (
+    Title,
+    Genre,
+    Category
+)
+
+
+class CategoriesGenresBaseViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    permission_classes = (AdminPermission | ReadOnlyPermission,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'slug'
+
+
+class CategoriesViewSet(CategoriesGenresBaseViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenresViewSet(CategoriesGenresBaseViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.annotate(rating=Avg('reviews__score'))
+    permission_classes = (AdminPermission | ReadOnlyPermission,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitlesFilter
+
+    def get_serializer_class(self):
+        if self.action == 'create' or self.action == 'partial_update':
+            return TitleWriteSerializer
+        return TitleReadSerializer
