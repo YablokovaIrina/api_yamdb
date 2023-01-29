ef4d4ed181711e309b242542972f25bbea50eb21
Yablokova Irina <yablokovairinav@yandex.ru>
2023-01-29 19:12:14 +0300

    +Модель произведение-жанр

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение',
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        verbose_name='Жанр',
+    )
